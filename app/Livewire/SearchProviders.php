09324c78ddd47b85ab305c66afca38d23f711758
joshwegener <JoshWegener@gmail.com>
2023-10-19 21:32:20 +0000

    Create API Service

--- app/Livewire/SearchProviders.php
+++ app/Livewire/SearchProviders.php
@@ -2,17 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Services\NpiApiService;
 use Livewire\Component;
 
 class SearchProviders extends Component
 {
     public $firstName;
 
-    public function render()
+    public function render(NpiApiService $api)
     {
         sleep(1); // simulate slow api call
 
-        $results = ['test 1', 'test 2']; // get results from api
+        $results = $api->searchProviders();
 
         return view('livewire.search-providers')->with(compact('results'));
     }
---
+++ app/Services/NpiApiService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class NpiApiService
+{
+    private $client;
+    private static $baseUri = 'https://npiregistry.cms.hhs.gov/api/';
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => self::$baseUri,
+        ]);
+    }
+
+    public function searchProviders(array $filters)
+    {
+        $params = [
+            'query' => [
+                'version' => '2.1',
+                'first_name' => $filters['first_name'] ?? '', // Default to empty string if not set
+                // Add other query parameters here
+            ]
+        ];
+
+        try {
+            $response = $this->client->request('GET', '', $params);
+            return json_decode($response->getBody(), true);
+        } catch (GuzzleException $e) {
+            // Handle exception or return a default failure status
+            return [
+                'status' => 'failure',
+                'message' => 'Failed to fetch data from NPI Registry',
+                'error' => $e->getMessage(),
+            ];
+        }
+    }
+}
