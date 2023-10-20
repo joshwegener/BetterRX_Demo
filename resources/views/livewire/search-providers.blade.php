56078ea6f3f42e84790d56e999dc88ffb85ff1cc
joshwegener <JoshWegener@gmail.com>
2023-10-20 16:26:35 +0000

    testing formate

--- resources/views/livewire/search-providers.blade.php
+++ resources/views/livewire/search-providers.blade.php
@@ -1,12 +1,29 @@
 <div>
     <form class="form-inline">
-        <input class="form-control mb-2 mr-sm-2" type="text" id="firstName" wire:model.live="firstName" placeholder="First Name">
+        <input class="form-control" type="text" id="firstName" wire:model.live="firstName" placeholder="First Name">
         <input class="form-control" type="text" id="lastName" wire:model.live="lastName" placeholder="Last Name">
         <input class="form-control" type="text" id="npiNumber" wire:model.live="npiNumber" placeholder="NPI Number">
         <input class="form-control" type="text" id="taxonomyDescription" wire:model.live="taxonomyDescription" placeholder="Taxonomy Description">
         <input class="form-control" type="text" id="city" wire:model.live="city" placeholder="City">
         <input class="form-control" type="text" id="state" wire:model.live="state" placeholder="State">
         <input class="form-control" type="text" id="zip" wire:model.live="zip" placeholder="Zip">
+    </form>
+
+    <form>
+        <div class="form-row">
+            <div class="col-md-4 mb-3">
+                <label for="firstName">First name</label>
+                <input type="text" class="form-control" id="firstName" placeholder="First name">
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="lastName">Last name</label>
+                <input type="text" class="form-control" id="lastName" placeholder="Last name">
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="npiNumber">NPI Number</label>
+                <input type="text" class="form-control" id="npiNumber" placeholder="NPI Number">
+            </div>
+        </div>
     </form>
 
     <table class="table table-striped">
