f5d677e0d43aee35da78dcbd521d738fda4d1f41
joshwegener <JoshWegener@gmail.com>
2023-10-21 23:42:55 +0000

    testing

--- app/Livewire/ModalLoader.php
+++ app/Livewire/ModalLoader.php
@@ -7,21 +7,18 @@ use Livewire\Component;
 
 class ModalLoader extends Component
 {
-    //public string $npiNumber = '';
+    public string $npiNumber = '';
     public array $providerData = [];
 
     protected $listeners = ['open-modal' => 'loadModal'];
 
     public function loadModal(string $npiNumber, NpiApiService $api)
-    //public function loadModal(array $providerData)
     {
-        // print debug info
-        //$this->npiNumber = $npiNumber;
+        $this->npiNumber = $npiNumber;
 
         $results = $api->searchProviders([ 'npiNumber' => $this->npiNumber ]);
         $this->providerData = $results[0];
         
-        //$this->providerData = $providerData;
         $this->dispatch('modal-open');
     }
 
