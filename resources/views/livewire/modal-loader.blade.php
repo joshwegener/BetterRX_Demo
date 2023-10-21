98a56312f37e91e974a9a5c0138056f2cd1a3be4
joshwegener <JoshWegener@gmail.com>
2023-10-21 20:53:23 +0000

    remove old modal

--- resources/views/livewire/modal-loader.blade.php
+++ resources/views/livewire/modal-loader.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ open: false }" @modal-open.window="open = true" style="display: none;" x-show="open" x-cloak>
+<!-- <div x-data="{ open: false }" @modal-open.window="open = true" style="display: none;" x-show="open" x-cloak>
     <div class="modal fade show" tabindex="-1" style="display: block; background-color: rgba(0,0,0,.5);" x-show="open" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
@@ -12,7 +12,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <div x-on:modal-open="open = true" x-show="open" class="modal" tabindex="-1">
   <div class="modal-dialog">
