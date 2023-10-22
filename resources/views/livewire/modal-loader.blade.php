57492d6a55c636c56f9da1c12d755cc8cd4697c1
joshwegener <JoshWegener@gmail.com>
2023-10-22 20:18:30 +0000

    display test

--- resources/views/livewire/modal-loader.blade.php
+++ resources/views/livewire/modal-loader.blade.php
@@ -6,16 +6,21 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        @if(count($providerData) > 0)
-          @if($providerData['enumeration_type'] == 'NPI-1')
-            {{ trim(implode(' ', array_filter([
-              $providerData['basic']['name_prefix'] ?? '',
-              $providerData['basic']['first_name'] ?? '',
-              $providerData['basic']['last_name'] ?? '',
-              ($providerData['basic']['name_suffix'] ?? '') !== '--' ? ($providerData['basic']['name_suffix'] ?? '') : '',
-              $providerData['basic']['credential'] ?? ''
-            ]))) }}
-          @endif
+          @if(count($providerData) > 0)
+          <div class="jumbotron text-center">
+            <h1 class="display-4">
+              @if($providerData['enumeration_type'] == 'NPI-1')
+                {{ trim(implode(' ', array_filter([
+                  ($providerData['basic']['name_prefix'] ?? '') !== '--' ? ($providerData['basic']['name_prefix'] ?? '') : '',
+                  $providerData['basic']['first_name'] ?? '',
+                  $providerData['basic']['last_name'] ?? '',
+                  ($providerData['basic']['name_suffix'] ?? '') !== '--' ? ($providerData['basic']['name_suffix'] ?? '') : '',
+                  $providerData['basic']['credential'] ?? ''
+                ]))) }}
+              @else
+              @endif
+            </h1>
+          </div>
           {{ var_dump($providerData) }}
         @endif
       </div>
