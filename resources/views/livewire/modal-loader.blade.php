8019035213abf82fc51b9f5bd0e708375fac7c3f
joshwegener <JoshWegener@gmail.com>
2023-10-22 20:03:31 +0000

    display name

--- resources/views/livewire/modal-loader.blade.php
+++ resources/views/livewire/modal-loader.blade.php
@@ -6,6 +6,9 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+        @if($providerData['enumeration_type' == 1])
+          {{ $providerData['basic']['name_prefix'] . ' ' . $providerData['basic']['first_name'] . ' ' . $providerData['basic']['last_name'] . ' ' . $providerData['basic']['name_suffix'] . ' ' . $providerData['basic']['credential'] }}
+        @endif
         {{ var_dump($providerData) }}
       </div>
     </div>
