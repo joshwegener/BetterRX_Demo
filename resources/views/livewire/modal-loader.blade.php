60a8935272cd0ce06a9241a85d82302fe6ebe376
joshwegener <JoshWegener@gmail.com>
2023-10-22 17:00:41 +0000

    modal testing

--- resources/views/livewire/modal-loader.blade.php
+++ resources/views/livewire/modal-loader.blade.php
@@ -1,4 +1,4 @@
-<div class="modal" id="providerDetails" tabindex="-1" aria-labelledby="providerDetailsLabel" aria-hidden="true" x-data="{ isOpen: false }" @modal-open="isOpen = true; alert($wire.isOpen)" x-show="$wire.isOpen">
+<div class="modal fade" id="providerDetails" tabindex="-1" aria-labelledby="providerDetailsLabel" aria-hidden="true" x-data="{ isOpen: false }" @modal-open="isOpen = true; alert($wire.isOpen)" x-show="$wire.isOpen">
   <div class="modal-dialog" x-show="$wire.isOpen">
     <div class="modal-content">
       <div class="modal-header">
@@ -7,6 +7,24 @@
       </div>
       <div class="modal-body">
         {{ var_dump($providerData) }}
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal" tabindex="-1" x-show="$wire.isOpen">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
